c07a5d0bed4218fb439f5e1cf018a81aca9fb6e4
cupcakemafa <hello@cupcakemafa.com>
2016-01-31 15:09:48 +0900

    Add resource server url.

--- blogger-theme3/js/app.js
+++ blogger-theme3/js/app.js
@@ -4,59 +4,66 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module( 'starter', [ 'ionic', 'starter.controllers' ] )
+    .constant( 'Const', {
+        'RESOURCE_SERVER_URL': '//cupcakemafa.github.io/blogger-theme3/'
+    } )
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-  });
-})
+    .run( function ( $ionicPlatform ) {
+        $ionicPlatform.ready( function () {
+        } );
+    } )
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    .config( function ( $stateProvider, $urlRouterProvider, Const ) {
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: '/templates/menu.html',
-    controller: 'AppCtrl'
-  })
+        //var resourceServerUrl = location.protocol + '//cupcakemafa.github.io/blogger-theme3/';
+        var resourceServerUrl = location.protocol + Const.RESOURCE_SERVER_URL;
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: '/templates/search.html'
-      }
-    }
-  })
+        $stateProvider
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: '/templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: '/templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+            .state( 'app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: resourceServerUrl + 'templates/menu.html',
+                controller: 'AppCtrl'
+            } )
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: '/templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+            .state( 'app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: '/templates/search.html'
+                    }
+                }
+            } )
+
+            .state( 'app.browse', {
+                url: '/browse',
+                views: {
+                    'menuContent': {
+                        templateUrl: '/templates/browse.html'
+                    }
+                }
+            } )
+            .state( 'app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                        templateUrl: '/templates/playlists.html',
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            } )
+
+            .state( 'app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: '/templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            } );
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise( '/app/playlists' );
+    } );
