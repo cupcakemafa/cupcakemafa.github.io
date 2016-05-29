a34a4af8e7eeebd144a9f30b2efd002b45ac5d22
Cupcakemafa <hello@cupcakemafa.com>
2016-05-29 12:48:26 +0900

    Debug

--- blogger-theme3/js/app.js
+++ blogger-theme3/js/app.js
@@ -1195,7 +1195,7 @@
                     $('#lucky-word-' + looper[i]).attr('src', word[i].img).attr('alt', word[i].text);
                     hush_tag = word[i].text.replace(/ /g, '_');
                     share_url = TWITTER_SHARE_PREFIX + word[i].text.replace(/%20/g, '_') + TWITTER_SHARE_POSTFIX;
-//                    share_url = encodeURIComponent(share_url);
+                    share_url = encodeURIComponent(share_url);
                     share_url = TWITTER_SHARE_BASE + share_url;
 console.info('share_url', share_url);
                     share_tag = '<a href="https://twitter.com/share" class="twitter-share-button" data-url="' + SHARE_URL + '" data-text="#' + hush_tag + '" data-via="' + SHARE_VIA + '" data-size="large" data-related="' + SHARE_VIA + '" data-hashtags="' + HUSH_TAG + '" data-dnt="false">Tweet</a>';
