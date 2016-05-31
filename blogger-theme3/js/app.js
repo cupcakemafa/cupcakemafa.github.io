6589ec6094ca4a1aff498400ec13cb87c238199f
Cupcakemafa <hello@cupcakemafa.com>
2016-05-31 23:45:17 +0900

    Set equal height

--- blogger-theme3/js/app.js
+++ blogger-theme3/js/app.js
@@ -513,7 +513,7 @@
                 perPage = 0;
             // Checking Post Max Count
             // Get label string from location URL
-            if (activePageUrl.indexOf("/search") != -1 || activePageUrl.indexOf("/search/label/") != -1) {
+            if (activePageUrl.indexOf('/search') != -1 || activePageUrl.indexOf('/search/label/') != -1) {
                 if (activePageUrl.indexOf("?updated-max") != -1) {
                     postLabel = activePageUrl.substring(activePageUrl.indexOf(labelSearchURI) + labelSearchURI.length, activePageUrl.indexOf("?updated-max"));
                 } else if (activePageUrl.indexOf("?updated-max") != -1) {
@@ -582,97 +582,97 @@
                 }
             });
         },
-        // Pager for index, archive page
-        pageListNavi = function (o) {
-            var pageNaviConf = {
-                    "perPage": 8,
-                    "numPages": 4,
-                    "firstText": " First ",
-                    "lastText": " Last ",
-                    "nextText": " &gt; ",
-                    "prevText": " &lt; "
-                },
-                m = location.href,
-                l = m.indexOf("/search/label/") !== -1,
-                a = l ? m.substr(m.indexOf("/search/label/") + 14, m.length) : '',
-                b = '',
-                g, k, e, n, h,
-                d, c, b, f,
-                show = function (f, e, a) {
-                    var d = Math.floor((pageNaviConf.numPages - 1) / 2),
-                        g = pageNaviConf.numPages - 1 - d,
-                        c = e - d,
-                        endPage;
-                    if (c <= 0) {
-                        c = 1;
-                    }
-                    endPage = e + g;
-                    if ((endPage - c) < pageNaviConf.numPages) {
-                        endPage = c + pageNaviConf.numPages - 1;
-                    }
-                    if (endPage > a) {
-                        endPage = a;
-                        c = a - pageNaviConf.numPages + 1;
-                    }
-                    if (c <= 0) {
-                        c = 1;
-                    }
-                    b = '<p class="pages padding">Pages ' + e + ' of ' + a + "</p> ";
-                    b += '<div class="btn-group btn-group-lg" role="group">';
-                    if (e > 1) {
-                        b += '<a class="btn btn-default" href="' + f[e - 1] + '">' + pageNaviConf.prevText + "</a>";
-                    }
-                    for (var i = c; i <= endPage; ++i) {
-                        if (i == e) {
-                            b += '<a class="btn btn-info current">' + i + "</a>";
-                        } else {
-                            b += '<a class="btn btn-default" href="' + f[i] + '">' + i + "</a>";
-                        }
-                    }
-                    if (e < a) {
-                        b += '<a class="btn btn-default" href="' + f[e + 1] + '">' + pageNaviConf.nextText + "</a>";
-                    }
-                    b += '</div>';
-
-                    b += '<div class="margin">';
-                    if (c > 1) {
-                        b += '<a class="btn btn-default btn-lg" href="' + f[1] + '">' + pageNaviConf.firstText + "</a>";
-                    }
-                    if (endPage < a) {
-                        b += ' <a class="btn btn-default btn-lg" href="' + f[a] + '">' + pageNaviConf.lastText + "</a>";
-                    }
-                    b += '</div>';
-
-                    $('.page-navi').html(b).removeClass('hidden');
-                };
-
-            if (o && o.feed && o.feed.entry && o.feed.entry.length) {
-                a = a.indexOf("?") !== -1 ? a.substr(0, a.indexOf("?")) : a;
-                g = l ? "/search/label/" + a + "?updated-max=" : "/search?updated-max=";
-                k = o.feed.entry.length;
-                e = Math.ceil(k / pageNaviConf.perPage);
-                if (e <= 1) {
-                    return;
-                }
-                n = 1;
-                h = [''];
-                l ? h.push("/search/label/" + a + "?max-results=" + pageNaviConf.perPage) : h.push("/?max-results=" + pageNaviConf.perPage);
-                for (d = 2; d <= e; d++) {
-                    c = (d - 1) * pageNaviConf.perPage - 1;
-                    b = o.feed.entry[c].published.$t;
-                    f = b.substring(0, 19) + b.substring(23, 29);
-                    //f = encodeURIComponent(f);
-                    //f = encodeURI(f);
-                    m = decodeURIComponent(m);
-                    if (m.indexOf(f) !== -1) {
-                        n = d;
-                    }
-                    f = encodeURIComponent(f);
-                    h.push(g + f + "&max-results=" + pageNaviConf.perPage);
-                }
-                show(h, n, e);
-            }
-        },
+//        // Pager for index, archive page
+//        pageListNavi = function (o) {
+//            var pageNaviConf = {
+//                    "perPage": 8,
+//                    "numPages": 4,
+//                    "firstText": " First ",
+//                    "lastText": " Last ",
+//                    "nextText": " &gt; ",
+//                    "prevText": " &lt; "
+//                },
+//                m = location.href,
+//                l = m.indexOf("/search/label/") !== -1,
+//                a = l ? m.substr(m.indexOf("/search/label/") + 14, m.length) : '',
+//                b = '',
+//                g, k, e, n, h,
+//                d, c, b, f,
+//                show = function (f, e, a) {
+//                    var d = Math.floor((pageNaviConf.numPages - 1) / 2),
+//                        g = pageNaviConf.numPages - 1 - d,
+//                        c = e - d,
+//                        endPage;
+//                    if (c <= 0) {
+//                        c = 1;
+//                    }
+//                    endPage = e + g;
+//                    if ((endPage - c) < pageNaviConf.numPages) {
+//                        endPage = c + pageNaviConf.numPages - 1;
+//                    }
+//                    if (endPage > a) {
+//                        endPage = a;
+//                        c = a - pageNaviConf.numPages + 1;
+//                    }
+//                    if (c <= 0) {
+//                        c = 1;
+//                    }
+//                    b = '<p class="pages padding">Pages ' + e + ' of ' + a + "</p> ";
+//                    b += '<div class="btn-group btn-group-lg" role="group">';
+//                    if (e > 1) {
+//                        b += '<a class="btn btn-default" href="' + f[e - 1] + '">' + pageNaviConf.prevText + "</a>";
+//                    }
+//                    for (var i = c; i <= endPage; ++i) {
+//                        if (i == e) {
+//                            b += '<a class="btn btn-info current">' + i + "</a>";
+//                        } else {
+//                            b += '<a class="btn btn-default" href="' + f[i] + '">' + i + "</a>";
+//                        }
+//                    }
+//                    if (e < a) {
+//                        b += '<a class="btn btn-default" href="' + f[e + 1] + '">' + pageNaviConf.nextText + "</a>";
+//                    }
+//                    b += '</div>';
+//
+//                    b += '<div class="margin">';
+//                    if (c > 1) {
+//                        b += '<a class="btn btn-default btn-lg" href="' + f[1] + '">' + pageNaviConf.firstText + "</a>";
+//                    }
+//                    if (endPage < a) {
+//                        b += ' <a class="btn btn-default btn-lg" href="' + f[a] + '">' + pageNaviConf.lastText + "</a>";
+//                    }
+//                    b += '</div>';
+//
+//                    $('.page-navi').html(b).removeClass('hidden');
+//                };
+//
+//            if (o && o.feed && o.feed.entry && o.feed.entry.length) {
+//                a = a.indexOf("?") !== -1 ? a.substr(0, a.indexOf("?")) : a;
+//                g = l ? "/search/label/" + a + "?updated-max=" : "/search?updated-max=";
+//                k = o.feed.entry.length;
+//                e = Math.ceil(k / pageNaviConf.perPage);
+//                if (e <= 1) {
+//                    return;
+//                }
+//                n = 1;
+//                h = [''];
+//                l ? h.push("/search/label/" + a + "?max-results=" + pageNaviConf.perPage) : h.push("/?max-results=" + pageNaviConf.perPage);
+//                for (d = 2; d <= e; d++) {
+//                    c = (d - 1) * pageNaviConf.perPage - 1;
+//                    b = o.feed.entry[c].published.$t;
+//                    f = b.substring(0, 19) + b.substring(23, 29);
+//                    //f = encodeURIComponent(f);
+//                    //f = encodeURI(f);
+//                    m = decodeURIComponent(m);
+//                    if (m.indexOf(f) !== -1) {
+//                        n = d;
+//                    }
+//                    f = encodeURIComponent(f);
+//                    h.push(g + f + "&max-results=" + pageNaviConf.perPage);
+//                }
+//                show(h, n, e);
+//            }
+//        },
         setSinglePageNavi = function () {
             var
                 $newerLink = $('a.next-link'),
@@ -747,40 +747,40 @@
                 }, "html");
             }
         },
-        // End Pager for index, archive page
-        setPageListNavi = function () {
-            var
-                $pageNavi = $('.page-navi'),
-                a, b, c, d,
-                //url = location.protocol + '//' + parser.hostname; //// @todo
-                url = SITE_URL; //// @debug
-            if ($pageNavi.length) {
-                b = location.href;
-                if (b.indexOf('?q=') !== -1 || b.indexOf('.html') !== -1) {
-                    return;
-                }
-                d = b.indexOf('/search/label/') + 14;
-                if (d !== 13) {
-                    c = b.indexOf("?"),
-                        a = (c === -1) ? b.substring(d) : b.substring(d, c);
-                    url += '/feeds/posts/summary/-/' + a + '?alt=json-in-script&max-results=99999';
-                } else {
-                    url += '/feeds/posts/summary?alt=json-in-script&max-results=99999';
-                }
-                //blogger API を実行する
-                $.ajax({
-                    type: 'GET',
-                    url: url,
-                    cache: false,
-                    data: {},
-                    dataType: 'jsonp'
-                }).done(function (json) {
-                    pageListNavi(json);
-                }).fail(function (error) {
-                    console.error('error', error);
-                });
-            }
-        },
+//        // End Pager for index, archive page
+//        setPageListNavi = function () {
+//            var
+//                $pageNavi = $('.page-navi'),
+//                a, b, c, d,
+//                //url = location.protocol + '//' + parser.hostname; //// @todo
+//                url = SITE_URL; //// @debug
+//            if ($pageNavi.length) {
+//                b = location.href;
+//                if (b.indexOf('?q=') !== -1 || b.indexOf('.html') !== -1) {
+//                    return;
+//                }
+//                d = b.indexOf('/search/label/') + 14;
+//                if (d !== 13) {
+//                    c = b.indexOf("?"),
+//                        a = (c === -1) ? b.substring(d) : b.substring(d, c);
+//                    url += '/feeds/posts/summary/-/' + a + '?alt=json-in-script&max-results=99999';
+//                } else {
+//                    url += '/feeds/posts/summary?alt=json-in-script&max-results=99999';
+//                }
+//                //blogger API を実行する
+//                $.ajax({
+//                    type: 'GET',
+//                    url: url,
+//                    cache: false,
+//                    data: {},
+//                    dataType: 'jsonp'
+//                }).done(function (json) {
+//                    pageListNavi(json);
+//                }).fail(function (error) {
+//                    console.error('error', error);
+//                });
+//            }
+//        },
         setRelatedPostNavi = function (labels) {
             var
             //url = location.protocol + '//' + parser.hostname, //// @todo
@@ -1280,17 +1280,25 @@
                 $elmTwo=$($listItem.eq(i+1));
                 heightOne = $elmOne.length ? parseInt($elmOne.height(), 10) : 0;
                 heightTwo = $elmTwo.length ? parseInt($elmTwo.height(), 10) : 0;
-                console.info(heightOne + ':' + heightTwo);
+                //console.info(heightOne + ':' + heightTwo);
                 if(heightOne && heightTwo) {
                     if(heightOne > heightTwo) {
                         heightOne+=ADJUST_BOTTOM;
                         heightTwo=0;
+                        $elmOne.height(heightOne);
                         $elmTwo.height(heightOne);
                     }
                     else if(heightTwo > heightOne) {
                         heightTwo+=ADJUST_BOTTOM;
                         heightOne=0;
                         $elmOne.height(heightTwo);
+                        $elmTwo.height(heightTwo);
+                    }
+                    else {
+                        heightOne+=ADJUST_BOTTOM;
+                        heightTwo+=ADJUST_BOTTOM;
+                        $elmOne.height(heightOne);
+                        $elmTwo.height(heightTwo);
                     }
                 }
             }
@@ -1336,7 +1344,7 @@
                 //setPageListNavi();
 
                 // Equal height for each .post-col.list-item
-                //setEqualHeight();
+                setEqualHeight();
 
                 // Set title
                 setArchiveTitle();
