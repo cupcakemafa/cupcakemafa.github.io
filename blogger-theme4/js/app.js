c14fba0061b6ee782f6624ade4fe8c56dade3150
Cupcakemafa <hello@cupcakemafa.com>
2016-08-20 17:19:37 +0900

    fix

--- blogger-theme4/js/app.js
+++ blogger-theme4/js/app.js
@@ -97,7 +97,7 @@ function setLuckyWord(_$) {
             share_tag = '<a href="https://twitter.com/share" ';
             share_tag += 'class="twitter-share-button" data-url="' + SHARE_URL + '" ';
             share_tag += 'data-text="' + getTweetText(lucky_word) + '" ';
-            share=tag += 'data-size="large" ';
+            share_tag += 'data-size="large" ';
 //            share_tag += 'data-via="' + SHARE_VIA + '" ';
 //            share_tag += 'data-related="' + SHARE_VIA + '" ';
 //            share_tag += 'data-hashtags="' + HUSH_TAG + '" ';
