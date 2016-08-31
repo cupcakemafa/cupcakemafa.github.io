8249efae8c898f9167915339036cc70f1c5d06c0
Cupcakemafa <hello@cupcakemafa.com>
2016-08-31 23:24:18 +0900

    Fix

--- blogger-theme4/js/app.js
+++ blogger-theme4/js/app.js
@@ -323,7 +323,7 @@ function setLuckyWord(_$) {
 
             $('#lucky-word-share-' + looper[i]).replaceWith(share_tag);
         }
-
+        /*
         // activate tweet button
         ! function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0],
@@ -335,7 +335,7 @@ function setLuckyWord(_$) {
                 fjs.parentNode.insertBefore(js, fjs);
             }
         }(document, 'script', 'twitter-wjs');
-
+        */
         var $open_lucky = _$('#open-lucky'),
             $lucky_header = _$('#lucky-header');
         $open_lucky.prop('disabled', false);
@@ -939,4 +939,17 @@ function disableImageLink($link) {
         $('.navbar-toggle:visible').click();
     });
 
+    // activate tweet button
+    ! function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            p = /^http:/.test(d.location) ? 'http' : 'https';
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + '://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, 'script', 'twitter-wjs');
+
 })(jQuery);
+
