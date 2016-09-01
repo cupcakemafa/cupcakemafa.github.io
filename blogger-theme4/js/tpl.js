1d26e87020efc75293180f1b4cd4f6da0d456b63
Cupcakemafa <hello@cupcakemafa.com>
2016-09-01 22:22:54 +0900

    debug

--- blogger-theme4/js/tpl.js
+++ blogger-theme4/js/tpl.js
@@ -488,7 +488,7 @@ function setLuckyWord(_$) {
 }
 this.on('mount', function() {
     setLuckyWord($);
-})(jQuery);
+});
 });
 
 riot.tag2('not-found', '<article class="post text-center"> <a href="/" title="Cupcakemafaのトップページへ"> <img class="img-circle" src="http://4.bp.blogspot.com/-VA6sLbbGX4I/VdmIpQk5I5I/AAAAAAAAAPM/GyDtlKiC4-0/s200/cupcakemafa-favicon-200x200.png"> </a> <h3>Sorry...</h3> <p>おさがしのページが<br>見つかりませんでした&#12290;</p> <p> <a href="/" class="btn btn-default" title="Cupcakemafaのトップページへ"> <i class="fa fa-home"></i> Cupcakemafaのトップページへ </a> </p> もしくは <div class="padding text-center"> <form action="/search" method="get" role="search"> <div class="input-group input-group-lg margin padding"> <input class="form-control" name="q" placeholder="サイト検索" type="search"> <span class="input-group-btn"> <button class="btn btn-default" type="button"> <i class="fa fa-search"></i> </button> </span> </div> </form> </div> </article>', '', '', function(opts) {
